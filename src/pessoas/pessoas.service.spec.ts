f743166197da98c440a9c021e862d17c0abe4a56
marcelofarias2010 <mljinformatica@gmail.com>
2026-01-16 14:56:28 -0300

    Finalizando os testes

---
+++ src/pessoas/pessoas.service.spec.ts
@@ -0,0 +1,266 @@
+import { Repository } from 'typeorm';
+import { PessoasService } from './pessoas.service';
+import { Pessoa } from './entities/pessoa.entity';
+import { HashingService } from 'src/auth/hashing/hashing.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { CreatePessoaDto } from './dto/create-pessoa.dto';
+import { ConflictException, NotFoundException } from '@nestjs/common';
+import { ForbiddenException } from '@nestjs/common';
+
+describe('PessoasService', () => {
+  let pessoasService: PessoasService;
+  let pessoaRepository: Repository<Pessoa>;
+  let hashingService: HashingService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        PessoasService,
+        {
+          provide: getRepositoryToken(Pessoa),
+          useValue: {
+            save: jest.fn(),
+            create: jest.fn(),
+            findOneBy: jest.fn(),
+            find: jest.fn(),
+            preload: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+        {
+          provide: HashingService,
+          useValue: {
+            hash: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    pessoasService = module.get<PessoasService>(PessoasService);
+    pessoaRepository = module.get<Repository<Pessoa>>(
+      getRepositoryToken(Pessoa),
+    );
+    hashingService = module.get<HashingService>(HashingService);
+  });
+
+  it('pessoasService deve estar definido', () => {
+    expect(pessoasService).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('deve criar uma nova pessoa', async () => {
+      // Arange
+      const createPessoaDto: CreatePessoaDto = {
+        email: 'luiz@email.com',
+        nome: 'Luiz',
+        password: '123456',
+      };
+      const passwordHash = 'HASHDESENHA';
+      const novaPessoa = {
+        id: 1,
+        nome: createPessoaDto.nome,
+        email: createPessoaDto.email,
+        passwordHash,
+      };
+      // Como o valor retornado por hashingService.hash é necessário
+      // vamos simular este valor.
+      jest.spyOn(hashingService, 'hash').mockResolvedValue(passwordHash);
+      // Como a pessoa retornada por pessoaRepository.create é necessária em
+      // pessoaRepository.save. Vamos simular este valor.
+      jest.spyOn(pessoaRepository, 'create').mockReturnValue(novaPessoa as any);
+
+      // Act -> Ação
+      const result = await pessoasService.create(createPessoaDto);
+
+      // Assert
+      // O método hashingService.hash foi chamado com createPessoaDto.password?
+      expect(hashingService.hash).toHaveBeenCalledWith(
+        createPessoaDto.password,
+      );
+      // O método pessoaRepository.create foi chamado com os dados da nova
+      // pessoa com o hash de senha gerado por hashingService.hash?
+      expect(pessoaRepository.create).toHaveBeenCalledWith({
+        nome: createPessoaDto.nome,
+        passwordHash,
+        email: createPessoaDto.email,
+      });
+
+      // O método pessoaRepository.save foi chamado com os dados da nova
+      // pessoa gerada por pessoaRepository.create?
+      expect(pessoaRepository.save).toHaveBeenCalledWith(novaPessoa);
+
+      // O resultado do método pessoasService.create retornou a nova
+      // pessoa criada?
+      expect(result).toEqual(novaPessoa);
+    });
+
+    it('deve lançar ConflictException quando e-mail já existe', async () => {
+      jest.spyOn(pessoaRepository, 'save').mockRejectedValue({
+        code: '23505',
+      });
+
+      await expect(pessoasService.create({} as any)).rejects.toThrow(
+        ConflictException,
+      );
+    });
+
+    it('deve lançar um erro genérico quando erro for lançado', async () => {
+      jest
+        .spyOn(pessoaRepository, 'save')
+        .mockRejectedValue(new Error('Erro genérico'));
+
+      await expect(pessoasService.create({} as any)).rejects.toThrow(
+        new Error('Erro genérico'),
+      );
+    });
+  });
+
+  describe('findOne', () => {
+    it('deve retornar uma pessoa se a pessoa for encontrada', async () => {
+      const pessoaId = 1;
+      const pessoaEncontrada = {
+        id: pessoaId,
+        nome: 'Luiz',
+        email: 'luiz@email.com',
+        passwordHash: '123456',
+      };
+
+      jest
+        .spyOn(pessoaRepository, 'findOneBy')
+        .mockResolvedValue(pessoaEncontrada as any);
+
+      const result = await pessoasService.findOne(pessoaId);
+
+      expect(result).toEqual(pessoaEncontrada);
+    });
+
+    it('deve lançar um erro se a pessoa não for encontrada', async () => {
+      await expect(pessoasService.findOne(1)).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+  });
+
+  describe('findAll', () => {
+    it('deve retornar todas as pessoas', async () => {
+      const pessoasMock: Pessoa[] = [
+        {
+          id: 1,
+          nome: 'Luiz',
+          email: 'luiz@email.com',
+          passwordHash: '123456',
+        } as Pessoa,
+      ];
+
+      jest.spyOn(pessoaRepository, 'find').mockResolvedValue(pessoasMock);
+
+      const result = await pessoasService.findAll();
+
+      expect(result).toEqual(pessoasMock);
+      expect(pessoaRepository.find).toHaveBeenCalledWith({
+        order: {
+          id: 'desc',
+        },
+      });
+    });
+  });
+
+  describe('update', () => {
+    it('deve atualizar uma pessoa se for autorizado', async () => {
+      // Arrange
+      const pessoaId = 1;
+      const updatePessoaDto = { nome: 'Joana', password: '654321' };
+      const tokenPayload = { sub: pessoaId } as any;
+      const passwordHash = 'HASHDESENHA';
+      const updatedPessoa = { id: pessoaId, nome: 'Joana', passwordHash };
+
+      jest.spyOn(hashingService, 'hash').mockResolvedValueOnce(passwordHash);
+      jest
+        .spyOn(pessoaRepository, 'preload')
+        .mockResolvedValue(updatedPessoa as any);
+      jest
+        .spyOn(pessoaRepository, 'save')
+        .mockResolvedValue(updatedPessoa as any);
+
+      // Act
+      const result = await pessoasService.update(
+        pessoaId,
+        updatePessoaDto,
+        tokenPayload,
+      );
+
+      // Assert
+      expect(hashingService.hash).toHaveBeenCalledWith(
+        updatePessoaDto.password,
+      );
+      expect(pessoaRepository.preload).toHaveBeenCalledWith({
+        id: pessoaId,
+        nome: updatePessoaDto.nome,
+        passwordHash,
+      });
+      expect(pessoaRepository.save).toHaveBeenCalledWith(updatedPessoa);
+      expect(result).toEqual(updatedPessoa);
+    });
+  });
+
+  describe('remove', () => {
+    it('deve remover uma pessoa se autorizado', async () => {
+      // Arrange
+      const pessoaId = 1; // Pessoa com ID 1
+      const tokenPayload = { sub: pessoaId } as any; // Usuário com ID 1
+      const existingPessoa = { id: pessoaId, nome: 'John Doe' }; // Pessoa é o Usuário
+
+      // findOne do service vai retornar a pessoa existente
+      jest
+        .spyOn(pessoasService, 'findOne')
+        .mockResolvedValue(existingPessoa as any);
+      // O método remove do repositório também vai retornar a pessoa existente
+      jest
+        .spyOn(pessoaRepository, 'remove')
+        .mockResolvedValue(existingPessoa as any);
+
+      // Act
+      const result = await pessoasService.remove(pessoaId, tokenPayload);
+
+      // Assert
+      // Espero que findOne do pessoaService seja chamado com o ID da pessoa
+      expect(pessoasService.findOne).toHaveBeenCalledWith(pessoaId);
+      // Espero que o remove do repositório seja chamado com a pessoa existente
+      expect(pessoaRepository.remove).toHaveBeenCalledWith(existingPessoa);
+      // Espero que a pessoa apagada seja retornada
+      expect(result).toEqual(existingPessoa);
+    });
+
+    it('deve lançar ForbiddenException se não autorizado', async () => {
+      // Arrange
+      const pessoaId = 1; // Pessoa com ID 1
+      const tokenPayload = { sub: 2 } as any; // Usuário com ID 2
+      const existingPessoa = { id: pessoaId, nome: 'John Doe' }; // Pessoa NÃO é o Usuário
+
+      // Espero que o findOne seja chamado com pessoa existente
+      jest
+        .spyOn(pessoasService, 'findOne')
+        .mockResolvedValue(existingPessoa as any);
+
+      // Espero que o servico rejeite porque o usuário é diferente da pessoa
+      await expect(
+        pessoasService.remove(pessoaId, tokenPayload),
+      ).rejects.toThrow(ForbiddenException);
+    });
+
+    it('deve lançar NotFoundException se a pessoa não for encontrada', async () => {
+      const pessoaId = 1;
+      const tokenPayload = { sub: pessoaId } as any;
+
+      // Só precisamos que o findOne lance uma exception e o remove também deve lançar
+      jest
+        .spyOn(pessoasService, 'findOne')
+        .mockRejectedValue(new NotFoundException());
+
+      await expect(
+        pessoasService.remove(pessoaId, tokenPayload),
+      ).rejects.toThrow(NotFoundException);
+    });
+  });
+});
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,25 +1,292 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, HttpStatus, ValidationPipe } from '@nestjs/common';
 import request from 'supertest';
-import { App } from 'supertest/types';
-import { AppModule } from '../src/app/app.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { RecadosModule } from 'src/recados/recados.module';
+import { PessoasModule } from 'src/pessoas/pessoas.module';
+import { GlobalConfigModule } from 'src/global-config/global-config.module';
+import * as path from 'path';
+import { AppModule } from 'src/app/app.module';
+import { ParseIntIdPipe } from 'src/common/pipes/parse-int-id.pipe';
+import { Test, TestingModule } from '@nestjs/testing';
+import { CreatePessoaDto } from 'src/pessoas/dto/create-pessoa.dto';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+const login = async (
+  app: INestApplication,
+  email: string,
+  password: string,
+) => {
+  const response = await request(app.getHttpServer())
+    .post('/auth')
+    .send({ email, password });
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+  return response.body.accessToken;
+};
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
+const createUserAndLogin = async (app: INestApplication) => {
+  const nome = 'Any User';
+  const email = 'anyuser@email.com';
+  const password = '123456';
+
+  await request(app.getHttpServer()).post('/pessoas').send({
+    nome,
+    email,
+    password,
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  return login(app, email, password);
+};
+
+describe('PessoasController (e2e)', () => {
+  let app: INestApplication;
+  let authToken: string;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        // LINHA REMOVIDA: ConfigModule.forFeature(globalConfig),
+        // Mantemos o GlobalConfigModule pois ele existe na sua pasta
+        GlobalConfigModule,
+        ConfigModule.forRoot({
+          // Adicione isso se precisar carregar variáveis de ambiente .env no teste
+          isGlobal: true,
+          envFilePath: '.env',
+        }),
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: 'localhost',
+          port: 5432,
+          username: 'postgres',
+          database: 'postgres',
+          password: '835221',
+          autoLoadEntities: true,
+          synchronize: true,
+          dropSchema: true,
+        }),
+        ServeStaticModule.forRoot({
+          rootPath: path.resolve(__dirname, '..', '..', 'pictures'),
+          serveRoot: '/pictures',
+        }),
+        RecadosModule,
+        PessoasModule,
+        AppModule,
+      ],
+    }).compile();
+
+    app = module.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: false,
+      }),
+      new ParseIntIdPipe(),
+    );
+    await app.init();
+
+    authToken = await createUserAndLogin(app);
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+
+  describe('POST /pessoas', () => {
+    it('deve criar uma pessoa sem erros', async () => {
+      const createPessoaDto: CreatePessoaDto = {
+        email: 'joao@email.com',
+        nome: 'João',
+        password: '123456',
+      };
+      const response = await request(app.getHttpServer())
+        .post('/pessoas')
+        .send(createPessoaDto)
+        .expect(HttpStatus.CREATED);
+
+      expect(response.body).toEqual({
+        active: true,
+        createdAt: expect.any(String),
+        email: createPessoaDto.email,
+        id: expect.any(Number),
+        nome: createPessoaDto.nome,
+        passwordHash: expect.any(String),
+        picture: '',
+        updatedAt: expect.any(String),
+      });
+    });
+
+    it('deve gerar um erro de e-mail já existe', async () => {
+      const createPessoaDto: CreatePessoaDto = {
+        email: 'joao2@email.com',
+        nome: 'João',
+        password: '123456',
+      };
+
+      await request(app.getHttpServer())
+        .post('/pessoas')
+        .send(createPessoaDto)
+        .expect(HttpStatus.CREATED);
+
+      const response = await request(app.getHttpServer())
+        .post('/pessoas')
+        .send(createPessoaDto)
+        .expect(HttpStatus.CONFLICT);
+
+      expect(response.body.message).toBe('E-mail já está cadastrado.');
+    });
+
+    it('deve gerar um erro de senha curta', async () => {
+      const createPessoaDto: CreatePessoaDto = {
+        email: 'joao2@email.com',
+        nome: 'João',
+        password: '123', // Este campo é inválido
+      };
+
+      const response = await request(app.getHttpServer())
+        .post('/pessoas')
+        .send(createPessoaDto)
+        .expect(HttpStatus.BAD_REQUEST);
+
+      expect(response.body.message).toEqual([
+        'password must be longer than or equal to 5 characters',
+      ]);
+    });
+  });
+
+  describe('GET /pessoas', () => {
+    it('deve retornar todas as pessoas', async () => {
+      await request(app.getHttpServer())
+        .post('/pessoas')
+        .send({
+          email: 'joao2@email.com',
+          nome: 'João',
+          password: '123456',
+        })
+        .expect(HttpStatus.CREATED);
+
+      const response = await request(app.getHttpServer())
+        .get('/pessoas')
+        .set('Authorization', `Bearer ${authToken}`)
+        .expect(HttpStatus.OK);
+
+      expect(response.body).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            id: expect.any(Number),
+            email: 'joao2@email.com',
+            nome: 'João',
+          }),
+        ]),
+      );
+    });
+  });
+
+  describe('GET /pessoas/:id', () => {
+    it('deve retornar uma pessoa pelo ID', async () => {
+      const createResponse = await request(app.getHttpServer())
+        .post('/pessoas')
+        .send({
+          email: 'joao2@email.com',
+          nome: 'João',
+          password: '123456',
+        })
+        .expect(HttpStatus.CREATED);
+
+      const personId = createResponse.body.id;
+
+      const response = await request(app.getHttpServer())
+        .get(`/pessoas/${personId}`)
+        .set('Authorization', `Bearer ${authToken}`)
+        .expect(HttpStatus.OK);
+
+      expect(response.body).toEqual(
+        expect.objectContaining({
+          id: personId,
+          email: 'joao2@email.com',
+          nome: 'João',
+        }),
+      );
+    });
+
+    it('deve retornar erro para pessoa não encontrada', async () => {
+      await request(app.getHttpServer())
+        .get('/pessoas/9999') // ID fictício
+        .set('Authorization', `Bearer ${authToken}`)
+        .expect(HttpStatus.NOT_FOUND);
+    });
+  });
+
+  describe('PATCH /pessoas/:id', () => {
+    it('deve atualizar uma pessoa', async () => {
+      const createResponse = await request(app.getHttpServer())
+        .post('/pessoas')
+        .send({
+          email: 'joao2@email.com',
+          nome: 'João',
+          password: '123456',
+        })
+        .expect(HttpStatus.CREATED);
+
+      const personId = createResponse.body.id;
+
+      const authToken = await login(app, 'joao2@email.com', '123456');
+
+      const updateResponse = await request(app.getHttpServer())
+        .patch(`/pessoas/${personId}`)
+        .send({
+          nome: 'João Atualizado',
+        })
+        .set('Authorization', `Bearer ${authToken}`)
+        .expect(HttpStatus.OK);
+
+      expect(updateResponse.body).toEqual(
+        expect.objectContaining({
+          id: personId,
+          nome: 'João Atualizado',
+        }),
+      );
+    });
+
+    it('deve retornar erro para pessoa não encontrada', async () => {
+      await request(app.getHttpServer())
+        .patch('/pessoas/9999') // ID fictício
+        .send({
+          nome: 'Nome Atualizado',
+        })
+        .set('Authorization', `Bearer ${authToken}`)
+        .expect(HttpStatus.NOT_FOUND);
+    });
+  });
+
+  describe('DELETE /pessoas/:id', () => {
+    it('deve remover uma pessoa', async () => {
+      const createResponse = await request(app.getHttpServer())
+        .post('/pessoas')
+        .send({
+          email: 'joao2@email.com',
+          nome: 'João',
+          password: '123456',
+        })
+        .expect(HttpStatus.CREATED);
+
+      const authToken = await login(app, 'joao2@email.com', '123456');
+
+      const personId = createResponse.body.id;
+
+      const response = await request(app.getHttpServer())
+        .delete(`/pessoas/${personId}`)
+        .set('Authorization', `Bearer ${authToken}`)
+        .expect(HttpStatus.OK);
+
+      expect(response.body.email).toBe('joao2@email.com');
+    });
+
+    it('deve retornar erro para pessoa não encontrada', async () => {
+      await request(app.getHttpServer())
+        .delete('/pessoas/9999') // ID fictício
+        .set('Authorization', `Bearer ${authToken}`)
+        .expect(HttpStatus.NOT_FOUND);
+    });
   });
 });
